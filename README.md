f1062f66f054a14f5f8758d9a851e46b46859ae5
qloha <172928329+qloha@users.noreply.github.com>
2024-10-02 14:45:54 -0700

    test hehe

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # project-xd.github.io
-https://project-xd.github.io
+## The Official Raven-XD Config Website
+### How to use:
+Go to https://project-xd.github.io and find the config you want.
+Once you are there, click "Download Config".
+After that, move it into your keystrokes/profiles folder.
+Go in game, press right shift, profiles, load profiles and select the config that you just downloaded.
+### Where is the keystrokes/profiles located?
+The folder which you store profiles is in the following:
+Windows - Open file explorer, and type %Appdata%\.minecraft\keystrokes\profiles
+* others coming soon *
--- resources/js/main.js
+++ resources/js/main.js
@@ -30,10 +30,10 @@ const configs = [
     tags: ["Blatant", "BlocksMC", "recommended"],
     server: "BlocksMC.com",
     description: "Config for BlocksMC",
-    cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/assets/icons/blocksmc.png",
+    cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/resources/icons/blocksmc.png",
     screenshots: ["https://github.com/user-attachments/assets/36e8920e-336e-4b62-ae3f-6b9962920812", "https://github.com/user-attachments/assets/85637f18-9d6b-47d9-9dc2-cb8594aff585"],
-    downloadLink: "https://lootdest.org/s?abc853f3",
-    otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Get Forge"]],
+    downloadLink: "https://github.com/Project-XD/project-xd.github.io/master/resources/configs/BlocksMC-qloha.json",
+    otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Download Forge"]],
     colors: {
         text: "#000000", // Text
         accent: "#36fff5", // Accent
@@ -42,15 +42,15 @@ const configs = [
 },
 {
     id: 2,
-    name: "Legit",
+    name: "100% Legit",
     creator: "qloha",
     tags: ["Multi-Server", "recommended"],
-    server: "Any",
+    server: "Multi-Server",
     description: "A legit config that ", // Stolen from Spezz.exchange
     cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/assets/icons/any.png",
     screenshots: ["https://spezz.exchange/uploads/monthly_2023_09/menu.webp.8e9432567dc1832cbfc1aabbc28d21e8.webp", "https://spezz.exchange/uploads/monthly_2023_09/screenshot.webp.4d44057d55f05dfcf4dba55600a6e894.webp"],
-    downloadLink: "https://spezz.exchange/store/product/191-augustus-client-lifetime/",
-    otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Get Forge"]],
+    downloadLink: "https://github.com/Project-XD/project-xd.github.io/master/resources/configs/100% Legit-qloha.json",
+    otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Download Forge"]],
     colors: {
         text: "#000000", // Text
         accent: "#007BFF", // Accent
@@ -97,7 +97,7 @@ configs.forEach(config => {
         }
 
         configDiv.innerHTML = `
-            <img src="${config.cover}" alt="if you see this, firefox messed up (or qloha did lol)${config.name}">
+            <img src="${config.cover}" alt="qloha did an oopsie sorry -> ${config.name}">
             <h2>${config.name}</h2>
             <p>${config.server}</p>
             <p>${config.creator}</p>
