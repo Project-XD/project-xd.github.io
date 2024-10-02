17e1d5665e214ddaf4fd98eeebb82da8f7e7663b
qloha <172928329+qloha@users.noreply.github.com>
2024-10-01 20:37:10 -0700

    test

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ const presetColors = {
 const clients = [
 {
     id: 1,
-    name: "Raven XD",
-    price: "Free",
-    tags: ["Free", "Ghost", "Blatant", "Hypixel", "Forge", "recommended"],
-    game: "Minecraft",
-    description: "Raven XD is a forge mod based on Raven BS. It features multiple blatant features like Killaura or Scaffold but also less blatant ones.",
+    name: "BlocksMC",
+    creator: "qloha",
+    tags: ["Blatant", "BlocksMC", "recommended"],
+    server: "BlocksMC.com",
+    description: "Config for BlocksMC",
     cover: "https://raw.githubusercontent.com/xia-mc/Raven-XD/master/logo/logo.png",
     screenshots: ["https://github.com/user-attachments/assets/36e8920e-336e-4b62-ae3f-6b9962920812", "https://github.com/user-attachments/assets/85637f18-9d6b-47d9-9dc2-cb8594aff585"],
     downloadLink: "https://lootdest.org/s?abc853f3",
@@ -42,15 +42,15 @@ const clients = [
 },
 {
     id: 2,
-    name: "Augustus",
-    price: "80€",
-    tags: ["Multi-Server", "Intave", "Polar", "Ghost", "Blatant", "recommended"],
-    game: "Minecraft",
-    description: "Introducing Augustus, a Minecraft 1.8 Client known for its extensive features. Boasting over 209 modules and a staggering 13,500+ customizable settings, Augustus is renowned for its unparalleled configurability. This client successfully bypasses popular servers like GommeHD, PikaNetwork, Mineblaze, and Gamster and outwits anti-cheats such as Intave, Polar, Grim, and Vulcan. Additionally, Augustus offers online configurations, allowing users to fine-tune their settings quickly. Elevate your Minecraft gameplay with Augustus – the ultimate choice for those seeking customization and performance.  ", // Stolen from Spezz.exchange
+    name: "Legit",
+    creator: "qloha",
+    tags: ["Multi-Server", "recommended"],
+    server: "Any",
+    description: "A legit config that ", // Stolen from Spezz.exchange
     cover: "https://spezz.exchange/uploads/monthly_2023_09/Augustus_Logo.webp.21990eea9fead38fbc9edfbc54b0a112.webp",
     screenshots: ["https://spezz.exchange/uploads/monthly_2023_09/menu.webp.8e9432567dc1832cbfc1aabbc28d21e8.webp", "https://spezz.exchange/uploads/monthly_2023_09/screenshot.webp.4d44057d55f05dfcf4dba55600a6e894.webp"],
     downloadLink: "https://spezz.exchange/store/product/191-augustus-client-lifetime/",
-    otherButtons:[["https://luren.wiki/", "中国采购(Chinese Purchase)"]],
+    otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Get Forge"]],
     colors: {
         text: "#000000", // Text
         accent: "#007BFF", // Accent
@@ -65,13 +65,13 @@ let hideOutdated = true;
 document.addEventListener("DOMContentLoaded", () => {
 populateClients();
 populateTagFilter();
-populateGameFilter();
+populateServerFilter();
 });
 
 // Function to populate clients
 function populateClients(filteredTag = "all") {
 const selectedTag = document.getElementById('tagFilter').value;
-const selectedGame = document.getElementById('gameFilter').value;
+const selectedServer = document.getElementById('serverFilter').value;
 const clientList = document.getElementById('clientList');
 clientList.innerHTML = '';
 
@@ -87,7 +87,7 @@ clients.forEach(client => {
     }
 
     if ((selectedTag === "all" || client.tags.includes(selectedTag)) &&
-        (selectedGame === "all" || client.game === selectedGame)) {
+        (selectedServer === "all" || client.server === selectedServer)) {
         const clientDiv = document.createElement('div');
         clientDiv.className = 'client';
 
@@ -99,8 +99,8 @@ clients.forEach(client => {
         clientDiv.innerHTML = `
             <img src="${client.cover}" alt="if you see this, firefox messed up (or bipas did lol)${client.name}">
             <h2>${client.name}</h2>
-            <p>${client.game}</p>
-            <p>${client.price}</p>
+            <p>${client.server}</p>
+            <p>${client.creator}</p>
             <a href="client.html?id=${client.id}">View Client</a>
         `;
         clientList.appendChild(clientDiv);
@@ -129,19 +129,19 @@ allTags.forEach(tag => {
 });
 }
 
-// Function to populate game filter options
-function populateGameFilter() {
-const gameFilter = document.getElementById('gameFilter');
-let allGames = new Set();
+// Function to populate server filter options
+function populateServerFilter() {
+const serverFilter = document.getElementById('serverFilter');
+let allServers = new Set();
 clients.forEach(client => {
-    allGames.add(client.game);
+    allServers.add(client.server);
 });
 
-allGames.forEach(game => {
+allServers.forEach(server => {
     const option = document.createElement('option');
-    option.value = game;
-    option.textContent = game;
-    gameFilter.appendChild(option);
+    option.value = server;
+    option.textContent = server;
+    serverFilter.appendChild(option);
 });
 }
 
