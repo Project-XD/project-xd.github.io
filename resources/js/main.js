6b17246ae7d8e00d8510423c59baa2b8b8c5107f
qloha <172928329+qloha@users.noreply.github.com>
2024-11-01 18:13:11 -0700

    Update + Stardix-qloha

--- resources/js/main.js
+++ resources/js/main.js
@@ -30,7 +30,7 @@ const configs = [
     updated: "10/6/2024",
     tags: ["Blatant", "BlocksMC", "recommended"],
     anticheat: ["Verus", "Updated NCP"],
-    description: ["Semi blatant, everything bypasses, low/high ping support, and lasts 25-40mins!<b><br />BedAura - V<br />ChestStealer - P<br />Gui - END<br />InvManager - P<br />KillAura - R<br />Scaffold - LALT<br />Speed - X<br />Antivoid : U</b>"],
+    description: ["Semi blatant, everything bypasses, low/high ping support, and lasts 25-40mins!<b><br />BedAura - V<br />ChestStealer & InvManager - P<br />Gui - END<br />KillAura - R<br />Scaffold - LALT<br />Speed - X<br />Antivoid : U</b>"],
     cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/resources/icons/BlocksMC.png",
     ip: "play.BlocksMC.com",
     downloadLink: "https://github.com/user-attachments/files/17270291/BlocksMC-under.json",
@@ -99,10 +99,10 @@ const configs = [
     id: 5,
     name: "clk BMC",
     creator: "celakk",
-    updated: "10/16/2024",
+    updated: "11/1/2024",
     tags: ["Blatant", "recommended"],
     anticheat: ["Verus", "Updated NCP"],
-    description: ["Fine tuned config for flag less gameplay. Made for skywars.<b><br />GUI - RShift<br />Killaura - Q<br />Scaffold - V<br />ChestStealer - B<br />InvManager - B<br />BedAura - H<br />Speed - Z</b>"],
+    description: ["Fine tuned config for flag less gameplay. Made for skywars.<b><br />GUI - RShift<br />Killaura - Q<br />Scaffold - V<br />ChestStealer & InvManager - B<br />BedAura - H<br />Speed - Z</b>"],
     cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/resources/icons/BlocksMC.png",
     ip: "BlocksMC.com",
     downloadLink: "https://github.com/user-attachments/files/17602316/BlocksMC-clk.json",
@@ -138,10 +138,28 @@ const configs = [
     updated: "10/22/2024",
     tags: ["Ghost", "Blatant", "recommended"],
     anticheat: ["Polar"],
-    description: ["Killaura sometimes flags.<b><br />Killaura - R<br />Speed X<br />Scaffold Rmenu</b>"],
+    description: ["Killaura sometimes flags.<b><br />Killaura - R<br />Speed - X<br />Scaffold - RMenu</b>"],
     cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/resources/icons/pika.png",
     ip: "play.pika-network.net",
     downloadLink: "https://github.com/user-attachments/files/17484130/Pika-under.json",
+    otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Download Forge"], ["https://bstlar.com/3AL/Raven-XD", "Download Raven XD"]],
+    colors: {
+        text: "#000000", // Text
+        accent: "#36fff5", // Accent
+        header: "#f70cf0" // Header
+    }
+},
+{
+    id: 8,
+    name: "Stardix",
+    creator: "qloha",
+    updated: "11/1/2024",
+    tags: ["Blatant", "recommended"],
+    anticheat: ["Old Grim"],
+    description: ["Good bypasses. Make sure you are using the latest version of Raven XD with the updated StaffDetector to avoid possible staff bans. You can get my dev build [here](https://file.io/F8sEwbU0Mpn9).<b><br />Killaura - R<br />Speed - G<br />Scaffold - H<br />Velocity - V<br />NoFall - N<br />Fly & Blink - F<br />Visuals - Minus</b>"],
+    cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/resources/icons/stardix.png",
+    ip: "play.stardix.com",
+    downloadLink: "https://github.com/user-attachments/files/17605101/Stardix-qloha.json",
     otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Download Forge"], ["https://bstlar.com/3AL/Raven-XD", "Download Raven XD"]],
     colors: {
         text: "#000000", // Text
